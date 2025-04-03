bc1bfbde895cb3714990054417cda6997439e40f
matias <matiaslamela1992@gmail.com>
2025-04-03 18:22:15 -0300

    initial commit

---
+++ src/main/java/uade/api/tpo_p2_mn_grupo_03/TpoP2MnGrupo03Application.java
@@ -0,0 +1,13 @@
+package uade.api.tpo_p2_mn_grupo_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoP2MnGrupo03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpoP2MnGrupo03Application.class, args);
+    }
+
+}
