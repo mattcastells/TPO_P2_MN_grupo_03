7a50ea1bcae6619371f8c8fe73266446d1f186c8
LeandroPar <91615869+LeandroPar@users.noreply.github.com.>
2025-04-09 15:56:30 -0300

    Category update+delete completo

--- src/main/java/uade/api/tpo_p2_mn_grupo_03/controller/CategoryController.java
+++ src/main/java/uade/api/tpo_p2_mn_grupo_03/controller/CategoryController.java
@@ -2,6 +2,7 @@ package uade.api.tpo_p2_mn_grupo_03.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,5 +40,10 @@ public class CategoryController {
     @PatchMapping
     public ResponseEntity<CategoryResponseDTO> update(@RequestBody CategoryPatchRequestDTO categoryRequestDTO) {
         return ResponseEntity.ok(categoryService.update(categoryRequestDTO));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<CategoryResponseDTO> update(@RequestBody CategoryRequestDTO categoryRequestDTO) {
+        return ResponseEntity.ok(categoryService.delete(categoryRequestDTO));
     }
 } 
