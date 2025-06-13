8ca01d1d5e6856f3b10dc7463048f31491c0fcbe
Matias Lamela <matias.lamela@avenida.com>
2025-06-12 21:09:43 -0300

    DON JIJOOOOTE

--- src/main/java/uade/api/tpo_p2_mn_grupo_03/service/IOrderService.java
+++ src/main/java/uade/api/tpo_p2_mn_grupo_03/service/IOrderService.java
@@ -4,10 +4,12 @@ import uade.api.tpo_p2_mn_grupo_03.dto.request.CreateOrderRequestDTO;
 import uade.api.tpo_p2_mn_grupo_03.dto.response.OrderResponseDTO;
 import uade.api.tpo_p2_mn_grupo_03.model.User;
 
+import java.util.List;
+
 public interface IOrderService {
     // Define los métodos del servicio aquí
     OrderResponseDTO findById(Long id);
     // Otros métodos según sea necesario
     OrderResponseDTO createOrder(CreateOrderRequestDTO orderRequestDTO, User user);
-    
+    List<OrderResponseDTO> findByUserId(Long userId, Integer offset, Integer limit);
 } 
